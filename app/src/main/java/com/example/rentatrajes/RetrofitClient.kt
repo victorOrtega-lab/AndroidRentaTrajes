f4bf9e06cf45e07e28f6d39b70cdf336e7725912
Hector <hectorlucioholguin@gmail.com>
2025-11-19 18:58:44 -0600

    implementacion de modulo de rentas al proyecto conjunto

---
+++ app/src/main/java/com/example/rentatrajes/RetrofitClient.kt
@@ -0,0 +1,20 @@
+package com.example.rentatrajes
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    // Asegúrate que termina en "/"
+    private const val BASE_URL = "https://buyer-takes-layer-forward.trycloudflare.com/api2/"
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(ScalarsConverterFactory.create()) // 1) texto plano (e.g. "correcto")
+            .addConverterFactory(GsonConverterFactory.create())    // 2) JSON (GET list)
+            .build()
+            .create(ApiService::class.java)
+    }
+}
